72ea18b9af2e596da16a2c57ca03e3381a9e9832
Matthew John Cheetham <mjcheetham@outlook.com>
2026-07-02 10:43:56 +0100

    otp: add scaffolding for CLI using System.CommandLine
    Signed-off-by: Matthew John Cheetham <mjcheetham@outlook.com>

--- src/otp/Program.cs
+++ src/otp/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+
+var rootCommand = new RootCommand("Create and manage one-time passwords (OTPs).");
+
+return await CommandLineParser.Parse(rootCommand, args).InvokeAsync();
